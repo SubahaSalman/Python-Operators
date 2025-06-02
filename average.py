1382d4468dab7742421309889a7314241601cda2
SubahaSalman <subaha.salman@gmail.com>
2025-06-02 18:48:22 +0200

    average python operators

---
+++ average.py
@@ -0,0 +1,11 @@
+shelf1= 23
+shelf2= 42
+shelf3= 17
+shelf4= 9
+shelf5= 38
+
+sum = shelf1 + shelf2 + shelf3 + shelf4 + shelf5
+print("The sum of all the books on the shelves is", sum)
+
+average = sum/5
+print("The average books on a shelf is", average)
